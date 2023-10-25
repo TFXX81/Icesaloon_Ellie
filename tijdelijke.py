eb20a448068711d6319174dc6634a62ec73f02ba
TFXX81 <145128242+TFXX81@users.noreply.github.com>
2023-10-25 17:18:38 +0200

    bestand tijdelijke.py gecodeerd

---
+++ tijdelijke.py
@@ -0,0 +1,17 @@
+mijn_dictionary={
+    "aardbei" : 3 ,
+    "vanille" : 4 ,
+    "chocolade" : 5
+}
+aanbieding = (mijn_dictionary["aardbei"]*0.8)
+reclame_tekst = (f"Vandaag in de aanbieding: vanille ijs, 1 liter - slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+mijn_string = reclame_tekst4
+for el in mijn_string:
+    lengte_string = len (mijn_string)
+    if lengte_string <5:
+        print (el.lower())
+    else:
+        print (el.upper())
