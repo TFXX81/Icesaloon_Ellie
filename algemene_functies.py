524b214290d1f458e24f64eb61bb7196b7375a61
TFXX81 <145128242+TFXX81@users.noreply.github.com>
2023-12-06 14:34:38 +0100

    making_it_clean making making_it_clean

---
+++ algemene_functies.py
@@ -0,0 +1,9 @@
+def mijn_functie_1(a):
+      return a**2
+tot_de_tweede = mijn_functie_1(4)
+print (tot_de_tweede)
+
+def mijn_functie_2(a,b):
+      return a+b, a-b, a*b, a/b
+teruggeefwaardes = mijn_functie_2 (12,3)
+print (teruggeefwaardes)
